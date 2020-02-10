df09bc411f786f110d4781a6865949bb28fb00da
Juanjo Guirao <juan.jose.guirao@gmail.com>
2020-02-10 21:49:14 +0000

    Exercise Finished.

--- src/main/java/com/udacity/course3/exercise10/Application.java
+++ src/main/java/com/udacity/course3/exercise10/Application.java
@@ -1,11 +1,14 @@
 package com.udacity.course3.exercise10;
 
+import com.udacity.course3.exercise10.model.Member;
 import com.udacity.course3.exercise10.repository.MemberRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -20,14 +23,29 @@ public class Application {
         return args -> {
             // STEP 1: Define Member and MemberRepository first before changing this class
 
-
             // STEP 2: create a Member record
-
+            Member member = new Member();
+            member.setFirstName("Robert");
+            member.setLastName("Mondovi");
+            member.setAge(23);
+            member.setGender("male");
 
             // STEP 3: save the Member record
+            memberRepository.save(member);
 
+            // read the Member using member last name
+            Optional<Member> optionalMember = memberRepository.findByLastName("Mondovi");
 
-            // read the Member using memeber last name
+            if(optionalMember.isPresent()) {
+                System.err.println("Member: " + optionalMember.orElse(null));
+            }
+
+            Optional<Member> optionalMember2 = memberRepository.findById(optionalMember.orElse(null).getId());
+
+            if(optionalMember2.isPresent()) {
+                System.err.println("Member found by ID: " + optionalMember2.orElse(null));
+            }
+
         };
     }
 }
--- src/main/java/com/udacity/course3/exercise10/model/Member.java
+++ src/main/java/com/udacity/course3/exercise10/model/Member.java
@@ -1,4 +1,67 @@
 package com.udacity.course3.exercise10.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("members")
 public class Member {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
